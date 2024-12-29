ef686ba0dd539a9cd58144c78c12b3033ea96b64
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2024-12-29 20:34:34 +0800

    Update diy-part1-luci2305.sh

--- diy-part1-luci2305.sh
+++ diy-part1-luci2305.sh
@@ -20,13 +20,15 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
+
 
 
 # Add luci-app-openclash
-wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
-unzip -d package/openclash package/openclash.zip
-cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
-rm -rf package/openclash package/openclash.zip
+# wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
+# unzip -d package/openclash package/openclash.zip
+# cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+# rm -rf package/openclash package/openclash.zip
 
 # Update luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
