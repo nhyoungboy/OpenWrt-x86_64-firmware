c1d70f90032365b7f0b22b07186e512b6c4ab397
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2024-12-29 21:21:33 +0800

    Update diy-part1-luci2305.sh

--- diy-part1-luci2305.sh
+++ diy-part1-luci2305.sh
@@ -19,16 +19,20 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 
 
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+# git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+sed -i '$a\src-git mosdns https://github.com/sbwml/luci-app-mosdns' ./feeds.conf.default
 
 # onliner
-git clone --depth=1 https://github.com/danchexiaoyang/luci-app-onliner package/luci-app-onliner
+# git clone --depth=1 https://github.com/danchexiaoyang/luci-app-onliner package/luci-app-onliner
+sed -i '$a\src-git onliner https://github.com/danchexiaoyang/luci-app-onliner' ./feeds.conf.default
 
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+sed -i '$a\src-git poweroff https://github.com/WukongMaster/luci-app-poweroff' ./feeds.conf.default
 
 # 4-添加 OpenClash 插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+
 # git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
 # Add luci-app-openclash
 # wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
